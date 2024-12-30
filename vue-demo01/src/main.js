83780c413abcb83e69fb444c1443e891056e2f00
ZTL123Z <2746395413@qq.com>
2024-12-30 15:55:50 +0800

    全区注册组件

--- vue-demo01/src/main.js
+++ vue-demo01/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import HelloWorld from './components/HelloWorld.vue'
 Vue.config.productionTip = false
-
+Vue.component('HelloWorld', HelloWorld)
 new Vue({
   el: '#app',
   render: h => h(App),
